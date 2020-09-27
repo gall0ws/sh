3ed9ade7de40e31407e42bf332241b4ac270ba1b
gall0ws <g4ll0ws@gmail.com>
2020-09-27 10:54:33 +0200

    updates

--- pixup.sh
+++ pixup.sh
@@ -5,5 +5,11 @@ md5=`md5sum "$1" | awk '{print $1}'`
 ext=`echo "$1" | awk -F. '{print $NF}'`
 
 scp "$1" aleph0.pw:pix/$md5.$ext
+url=https://aleph0.pw/pix/$md5.$ext
 
-echo https://aleph0.pw/pix/$md5.$ext
+if [ ! -z "$DISPLAY" ]; then
+	xclip=$(which xclip 2>/dev/null)
+	test -x $xclip && echo $url | $xclip -i -selection clipboard
+fi
+
+echo $url
