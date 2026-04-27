d41f32161566f0da856943373c6b1fe1160b2b09
gall0ws <g4ll0ws@gmail.com>
2026-04-27 09:10:46 +0200

    rewrote install-sh

--- install-sh.sh
+++ install-sh.sh
@@ -1,16 +1,29 @@
 #!/bin/sh
 
-srcdir=${1-$HOME/src/sh}
-bindir=${2-$HOME/bin}
+bindir=${1-$HOME/bin}
+hash="md5 -q"
 
-test -d $srcdir || {
-	echo 'invalid srcdir' >&2 
+bins="a+.sh a-.sh flood.sh genpw.sh gg.sh monitor.sh pixup.sh q.sh spongemock.sh \
+           typespeed_hs_trim.sh "
+
+Darwin="lifecap.sh"
+FreeBSD="lock.sh nat.sh urxvt-setfont.sh xm.sh pkg_log.sh"
+Linux="docker_cleanup.sh lenovo_scroll.sh lock.sh nat.sh urxvt-setfont.sh xm.sh"
+OpenBSD="lock.sh nat.sh urxvt-setfont.sh xm.sh"
+
+
+test -d $bindir || {
+	echo 'invalid dstdir' >&2
 	exit 1
 }
 
-for i in `ls ${srcdir}/*.sh`; do
-	dst=${bindir}/$(basename $i | sed 's/.sh$//')
-	cat $i > $dst
-	chmod +x $dst
-	echo installed $dst
+eval bins+="$`uname`"
+
+for i in $bins; do
+	dst=${bindir}/`echo $i | sed 's/.sh$//'`
+        if [ ! -e "$dst" ] || [ `$hash $dst` != `$hash $i` ]; then
+            cp $i $dst
+	    chmod +x $dst
+	    echo installed $dst
+        fi
 done
