609fcfb11c9e73c248897a320a1176f5a491d7dd
gall0ws <g4ll0ws@gmail.com>
2026-05-03 03:31:20 +0200

    videolen.sh: minor update

--- videolen.sh
+++ videolen.sh
@@ -3,6 +3,7 @@
 
 mediainfo=/opt/homebrew/bin/mediainfo
 quiet=false
+cmdname=`basename "$0"`
 
 if [ "$1" = "-q" ]; then
     quiet=true
@@ -10,11 +11,18 @@ if [ "$1" = "-q" ]; then
 fi
 
 if [ $# = 0 ]; then
-    printf "usage: %s [-q] file ...\n" `basename $0` >&2
+    printf "usage: %s [-q] file ...\n" $cmdname >&2
     exit 1
 fi
 
 while [ $# != 0 ]; do
+    if [ -d "$1" ]; then
+        if ! $quiet; then
+            printf "%s: %s is a directory\n" $cmdname "$1" >&2
+        fi
+        shift
+        continue
+    fi
     if ! $quiet; then
         printf "%s:\t" `basename $1`
     fi
