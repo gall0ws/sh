d11a4a995dc5a8c31e0c3cb5463ee3940a4f6bd5
gall0ws <g4ll0ws@gmail.com>
2026-05-14 14:27:51 +0200

    q.sh: completely rewrote + introduced -a and -b options
    -a will set the open sign and -b the closed one, e.g.:
    
       % q -a \( -b \) hello
       (hello)
    
    Also improved parsing of the arguments, e.g.:
    
       % echo quote me | q -
       'quote me'
    
       % q -s ^ -- -hello-
       ^-hello-^

--- q.sh
+++ q.sh
@@ -1,19 +1,43 @@
 #!/bin/sh
 
-sign=\'
-args=`getopt s: $*`
+SIGN_A=${SIGN_A:-\'}
+SIGN_B=${SIGN_B:-$SIGN_A}
 
-set -- $args
+usage() {
+    echo "usage: $0 [ -s sign | -a open_sign -b close_sign ] [ text ... ]" >&2
+    exit 64
+}
+
+count=0
 while :; do
-	case "$1" in
-	-s)
-		sign=$2
-		shift; shift;
-		;;
-	--)
-		shift; break;
-		;;
-	esac
-done
+    getopts s:a:b: opt $@ || break
+    case $opt in
+        s)
+            SIGN_A=$OPTARG
+            SIGN_B=$OPTARG
+            let count+=2
+            ;;
+        a)
+            SIGN_A=$OPTARG
+            let count+=2
+            ;;
 
-echo ${sign}${@}${sign}
+        b)
+            SIGN_B=$OPTARG
+            let count+=2
+            ;;
+        \?)
+            usage
+            ;;
+    esac
+done
+shift $count
+
+if [ "$1" = "--" ]; then
+    shift
+fi
+if [ $# = 0 ] || [ $# = 1 -a "$1" = "-" ]; then
+    sed -r `printf 's/.*/%s&%s/' ${SIGN_A} ${SIGN_B}`
+else
+    echo ${SIGN_A}${@}${SIGN_B}
+fi
